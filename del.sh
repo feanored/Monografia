1e01fbb274b7fb4755f1db957544a282adf29484
feanored <eduardo.floyd@outlook.com>
2020-03-08 23:17:27 -0300

    V1

---
+++ del.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+rm -f *.gz
+rm -f *.aux
+rm -f *.blg
+rm -f *.fdb_latexmk
+rm -f *.fls
+rm -f *.idx
+rm -f *.ilg
+rm -f *.ind
+rm -f *.out
+rm -f *.bcf
+rm -f *.xml
+rm -f *.log
+rm -f *.ist
+rm -f *.xdy
+rm -f *.lo*
+rm -f *.toc
+rm -f *.bbl
